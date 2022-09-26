04a355f143221fcfda236164c8422bc13b7cf720
afj200 <angel.fuentejimenez@gmail.com>
2022-09-26 13:43:32 +0200

    more changes

--- src/Main/main.java
+++ src/Main/main.java
@@ -3,6 +3,7 @@ package Main;
 public class main {
 
 	 public static void main( String[] args ) {
-		 System.out.println("Hola Angel de la Fuente Jimenez");
+		 System.out.println("Hola Mundo");
+		 System.out.println("Hola Angel");
 	 }
 }
